457fec6b88d74ab9f007e938ae27929bca6bc771
Вадим Белинович <belinovich@Vadims-Mac-mini.local>
2025-03-10 02:41:58 +0300

    add dockerfile

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -53,7 +53,7 @@ export default function Hero() {
 						href="/Белинович%20Вадим%20Игоревич.pdf"
 						target="_blank"
 						className="inline-flex items-center justify-center whitespace-nowrap rounded-full text-sm font-semibold ring-offset-background transition-opacity focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-highlight-primary focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary hover:opacity-90 text-black px-8 py-3">
-						Мое резюме
+						Моё резюме
 					</Link>
 				</div>
 			</div>
